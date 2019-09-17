636b77a7dd17af4719e96598018990e9cadf4989
Bastian Boll <bastibboll@googlemail.com>
2019-09-17 16:59:20 +0200

    compress relations

--- ac/data/extract.py
+++ ac/data/extract.py
@@ -4,6 +4,7 @@ import numpy as np
 import sparse
 
 CHUNK_SIZE = int(1e6)
+RELATION_COUNT = 15
 
 def resolve_relations(db_file, rel_file, meta_file, id_file):
 	"""
@@ -11,28 +12,39 @@ def resolve_relations(db_file, rel_file, meta_file, id_file):
 	conn = open_db_connection(db_file)
 	c = conn.cursor()
 
-	# load current offset
+	# load or compute unique IDs
 	if os.path.isfile(meta_file):
 		meta = np.load(meta_file)
 		off = meta[0]
 		num_unique = meta[1]
+		unique_ids = np.load(id_file)
 	else:
 		off = 0
 		c.execute("SELECT DISTINCT event1_id FROM Relations;")
 		event_ids = set(c.fetchall())
-		print("fetched first")
 		for id2 in c.execute("SELECT event2_id FROM Relations;"):
 			if not id2 in event_ids:
 				event_ids.add(id2)
-		print("fetched second")
-		unique_ids = list(event_ids)
+		unique_ids = np.char.array(list(event_ids))
 		num_unique = len(event_ids)
-		np.save(id_file, np.char.array(unique_ids))
+		np.save(id_file, unique_ids)
 		np.save(meta_file, np.array([off, num_unique]))
 
-	# load (compressed) relations
-	# relations = np.load()
-
+	id_lookup = dict()
+	for i, id_entr in enumerate(unique_ids):
+		id_lookup[id_entr[0]] = i
+	
+	# load or compute (compressed) relations
+	if os.path.isfile(rel_file):
+		relations = sparse.load_npz(rel_file)
+	else:
+		relations = sparse.DOK((num_unique, num_unique, RELATION_COUNT), dtype=np.float32)
+		for row in c.execute("SELECT * FROM Relations;"):
+			id_out = row[1]
+			id_in = row[2]
+			relations[id_lookup[id_out], id_lookup[id_in], :] = row[3:]
+		relations = sparse.COO(relations)
+		sparse.save_npz(rel_file, relations)
 	conn.close()
 
 def open_db_connection(db_file):
